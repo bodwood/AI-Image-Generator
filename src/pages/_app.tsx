8545905ed722aad34b827220f26518dafcd347a6
bodie wood <bodie1@macbook-air.lan>
2023-05-25 14:26:36 -0700

    remove default styling from next.js, add new stylesheet with background color. Import stylesheet to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../pages/App.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='app-main'>
+      <h2>Create Images With Your Mind</h2>
+      <textarea
+        className='app-input'
+        placeholder='Create any type of image you can think of with as much added description as you would like.'
+      />
+      <button>Generate Image</button>
+    </div>
+  );
 }
